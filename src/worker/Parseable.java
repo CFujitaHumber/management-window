e76ac31f07aa6cd1acb2f94d41339eaf4dbf3d22
TheFujirose <TheFujirose@users.noreply.github.com>
2025-10-26 15:19:06 -0400

    parseable interface

---
+++ src/worker/Parseable.java
@@ -0,0 +1,26 @@
+/**
+ * @author Carson Fujita
+ */
+package worker;
+
+/**
+ * Denotes an contract that can be parsed from string and back.
+ */
+public interface Parseable {
+	/**
+	 * Parses the text into a {@link Parseable} instance.
+	 * @param text the text to be parsed. 
+	 * It's the direct opposite of {@link Parseable#toString() toString()}
+	 * @return a {@link Parseable} instance
+	 * @see Parseable#toString()
+	 */
+	Parseable parse(String text);	
+	
+	/**
+	 * Turns a {@link Parseable} instance
+	 * into a string
+	 * @return string that can be parsed
+	 * @see Parseable#parse(String)
+	 */
+	String toString();
+}
