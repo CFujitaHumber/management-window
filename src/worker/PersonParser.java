432c18c45f153bcb4ddc1ad91c52ddf5f3cb7fab
TheFujirose <TheFujirose@users.noreply.github.com>
2025-11-04 13:26:43 -0500

    person build interface

---
+++ src/worker/PersonParser.java
@@ -0,0 +1,41 @@
+/**
+ * @author Carson Fujita
+ */
+package worker;
+
+import java.text.ParseException;
+
+/**
+ * Contract defines the interface of a builder that works off of parsing
+ */
+public interface PersonParser {
+	
+	/**
+	 * So apparently I can't use a char because string splits use solely Strings...
+	 * So it's a string... Fortunately this should contain just 16-bits regardless.
+	 */
+	String DELIMITER= " ";
+
+	/**
+	 * Sets the name of the {@link worker.Person}
+	 */
+	void setName(String name);
+
+	/**
+	 * Sets the hours of a {@link worker.Person} by parsing
+	 * the specified hours String to a number.
+	 * precision is determined by the subclass. 
+	 * @param hours parseable string value 
+	 * @throws NumberFormatException
+	 * @throws ParseException
+	 */
+	void setHours(String hours) throws NumberFormatException, ParseException;
+
+	/**
+	 * Sets the role of a {@link worker.Person} by parsing
+	 * the specified role to whatever enum or type the subclass desires.
+	 * @param role parseable string value
+	 * @throws ParseException 
+	 */
+	void setRole(String role) throws ParseException;
+}
